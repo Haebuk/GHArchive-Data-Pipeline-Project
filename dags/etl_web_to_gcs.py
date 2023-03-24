529da7502bcc109850b52e1184f901614d709d5f
Jaesung Ryu <cwryu6252@naver.com>
2023-03-24 20:52:17 +0900

    maximum_object_size

--- dags/etl_web_to_gcs.py
+++ dags/etl_web_to_gcs.py
@@ -79,7 +79,7 @@ def etl_github_archive_data_to_gcs_dag():
                     actor,
                     repo,
                     strptime(created_at, '%Y-%m-%dT%H:%M:%SZ') as created_at,
-                from read_ndjson('{local_json_gz_path}', columns={DUCKDB_SCHEMA})
+                from read_ndjson('{local_json_gz_path}', columns={DUCKDB_SCHEMA}, maximum_object_size=2000000)
             """
         )
 
