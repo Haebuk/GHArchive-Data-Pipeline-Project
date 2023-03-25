ad742bcf1c6bc4ccb36e9dfaa80c9d26119f1cc3
Jaesung Ryu <cwryu6252@naver.com>
2023-03-25 11:14:46 +0900

    add date field

--- gh_dbt/models/marts/type_counts.sql
+++ gh_dbt/models/marts/type_counts.sql
@@ -1,6 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['date', 'type']
+    )
+}}
 select
+    format_date('%Y-%m-%d', date_trunc(created_at, day)) as date,
     type,
     count(*) as count
 from {{ ref('stg_gh_events') }}
-group by type
-order by count
+{% if is_incremental() %}
+
+    where created_at >= (select max(created_at) from {{ this }})
+
+{% endif %}
+group by date, type
+order by date desc, count desc
